186cdbf1642c5f05a4ccd63987c7ba8ae95bfaec
siddharthag24 <137896685+siddharthag24@users.noreply.github.com>
2024-08-07 22:14:01 +0530

    Create Reverse_string.cpp

---
+++ Reverse_string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s;
+cout<<"Enter a string to reverse: ";
+cin >> s;
+reverse(s.begin(), s.end());
+cout<< "reversed string: "<< s;
+}
